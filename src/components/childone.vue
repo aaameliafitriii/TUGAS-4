a63aaa11b7d52f90719bb8c9dd2bcba8378a8c25
amel <ameliaftr0912@gmail.com>
2024-05-28 10:16:37 +0700

    first commit

---
+++ src/components/childone.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="child-one">
+      <h3>Child One</h3>
+      <button @click="sendData">Send Data to Parent</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'childone',
+    methods: {
+      sendData() {
+        this.$emit('data-from-child', 'Data from child one');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .child-one {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+    text-align: center;
+  }
+  
+  .child-one h3 {
+    color: #2c3e50;
+    margin-bottom: 10px;
+  }
+  
+  .child-one button {
+    padding: 8px 16px;
+    background-color: #42b983;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  .child-one button:hover {
+    background-color: #358a68;
+  }
+  </style>
+  
---
+++ src/components/childtwo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h3>Child Two</h3>
+      <button @click="sendData">Send Data to Parent</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'childtwo',
+    methods: {
+      sendData() {
+        this.$emit('data-from-child', 'Data from child two');
+      }
+    }
+  }
+  </script>
